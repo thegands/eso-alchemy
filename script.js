8c12e1107aecfa0b42013177a7fc3d2e02935377
Gandhi Anderson <thegands@gmail.com>
2017-01-24 21:31:33 -0700

    add new reagents and fix layout

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ var alchemyEffects = {
   "srs": { "abbr": "+Res", "desc": "Increase Spell Resist", "positive": 1 },
   "wcr": { "abbr": "+WCr", "desc": "Weapon Crit", "positive": 1 },
   "wpt": { "abbr": "+Wep", "desc": "Increase Weapon Power", "positive": 1 },
+  "pro": { "abbr": "+Pro", "desc": "Protection", "positive": 1 },
+  "vit": { "abbr": "+Vit", "desc": "Vitality", "positive": 1 },
+  "shp": { "abbr": "+SHP", "desc": "Sustained Restore Health", "positive": 1 },
   "xar": { "abbr": "-Amr", "desc": "Lower Armor", "positive": 0 },
   "xhp": { "abbr": "-HP", "desc": "Ravage Health", "positive": 0 },
   "xmg": { "abbr": "-MP", "desc": "Ravage Magicka", "positive": 0 },
@@ -27,23 +30,34 @@ var alchemyEffects = {
   "xsr": { "abbr": "-Res", "desc": "Lower Spell Resist", "positive": 0 },
   "stu": { "abbr": "Stun", "desc": "Stun Player", "positive": 0 },
   "xwc": { "abbr": "-WCr", "desc": "Lower Weapon Crit", "positive": 0 },
-  "xwp": { "abbr": "-Wep", "desc": "Lower Weapon Power", "positive": 0 }
+  "xwp": { "abbr": "-Wep", "desc": "Lower Weapon Power", "positive": 0 },
+  "chp": { "abbr": "-CHP", "desc": "Creeping Ravage Health", "positive": 0 },
+  "def": { "abbr": "-Def", "desc": "Defile", "positive": 0 },
+  "vul": { "abbr": "-Vul", "desc": "Vulnerability", "positive": 0 }
 };
 var alchemyReagents = {
+  "bs": { "e1": "xsr", "e2": "pro", "e3": "arm", "e4": "vit", "img": "img/bs.png", "desc": "Beetle Scuttle" },
   "bt": { "e1": "sta", "e2": "xhp", "e3": "wpt", "e4": "spd", "img": "img/bt.png", "desc": "Blessed Thistle" }, //Verified
   "be": { "e1": "xmg", "e2": "hpr", "e3": "xsp", "e4": "inv", "img": "img/be.png", "desc": "Blue Entoloma" }, //Verified
   "bg": { "e1": "srs", "e2": "xsp", "e3": "hpr", "e4": "mag", "img": "img/bg.png", "desc": "Bugloss" }, //Verified
+  "bw": { "e1": "hpr", "e2": "shp", "e3": "xsc", "e4": "vit", "img": "img/bw.png", "desc": "Butterfly Wing" },
   "cl": { "e1": "hpr", "e2": "sta", "e3": "mag", "e4": "uns", "img": "img/cl.png", "desc": "Columbine" }, //Verified
   "cf": { "e1": "mag", "e2": "xhp", "e3": "spw", "e4": "det", "img": "img/cf.png", "desc": "Cornflower" }, //Verified
   "dt": { "e1": "wpt", "e2": "xar", "e3": "sta", "e4": "wcr", "img": "img/dt.png", "desc": "Dragonthorn" }, //Verified
   "er": { "e1": "xhp", "e2": "xmg", "e3": "xst", "e4": "stu", "img": "img/er.png", "desc": "Emetic Russala" }, //effect 3 not tested - checks out on esohead
+  "fl": { "e1": "xst", "e2": "vul", "e3": "chp", "e4": "vit", "img": "img/fl.png", "desc": "Fleshfly Larva" },
   "is": { "e1": "xwp", "e2": "arm", "e3": "xst", "e4": "xwc", "img": "img/is.png", "desc": "Imp Stool" }, //Verified
   "ls": { "e1": "spw", "e2": "xsr", "e3": "mag", "e4": "scr", "img": "img/ls.png", "desc": "Lady's Smock" }, //Verified
   "lr": { "e1": "xst", "e2": "xwp", "e3": "hpr", "e4": "xsd", "img": "img/lr.png", "desc": "Luminous Russula" }, //Verified
   "mf": { "e1": "arm", "e2": "xwp", "e3": "hpr", "e4": "sta", "img": "img/mf.png", "desc": "Moutain Flower" }, //Verified
+  "mc": { "e1": "srs", "e2": "arm", "e3": "pro", "e4": "def", "img": "img/mc.png", "desc": "Mudcrab Chitin" },
   "nr": { "e1": "scr", "e2": "inv", "e3": "spd", "e4": "uns", "img": "img/nr.png", "desc": "Namira's Rot" }, //effect 4 - checks out on esohead
+  "ns": { "e1": "xhp", "e2": "pro", "e3": "chp", "e4": "def", "img": "img/ns.png", "desc": "Nightshade" },
   "nn": { "e1": "xhp", "e2": "xwc", "e3": "xsc", "e4": "inv", "img": "img/nn.png", "desc": "Nirnroot" }, //verified
+  "sj": { "e1": "xmg", "e2": "spd", "e3": "vul", "e4": "shp", "img": "img/sj.png", "desc": "Scrib Jelly" },
+  "se": { "e1": "xsd", "e2": "inv", "e3": "shp", "e4": "def", "img": "img/se.png", "desc": "Spider Egg" },
   "sh": { "e1": "xar", "e2": "wpt", "e3": "xhp", "e4": "xst", "img": "img/sh.png", "desc": "Stinkhorn" }, //effect 4 not tested - checks out on esohead
+  "tt": { "e1": "xar", "e2": "xwc", "e3": "det", "e4": "vit", "img": "img/tt.png", "desc": "Torchbug Thorax" },
   "vc": { "e1": "xsr", "e2": "spw", "e3": "xhp", "e4": "xmg", "img": "img/vc.png", "desc": "Violet Coprinus" }, //verified
   "wh": { "e1": "hpr", "e2": "wcr", "e3": "scr", "e4": "stu", "img": "img/wh.png", "desc": "Water Hyacinth" }, //Verified
   "wc": { "e1": "xsp", "e2": "srs", "e3": "xmg", "e4": "xsc", "img": "img/wc.png", "desc": "Whitecap" }, //verified
@@ -52,12 +66,12 @@ var alchemyReagents = {
 
 var loadData = function() {
     clearDetail();
-    
+
     //Prep arrays
     for(code in alchemyEffects) {
         alchemyEffects[code].reagents = new Array();
     }
-    
+
     //Init variables
     $("#selected").data("num", 0);
     $("#reagents").data("mode", "use");
@@ -87,22 +101,22 @@ var loadData = function() {
         //Close
         rdiv += "</span><br /><span class='name'>" + reagent.desc + "</span>";
         rdiv += "</div>";
-        
+
         $("#reagents").append(rdiv);
         $("#r"+code).data("code",code);
         $("#r"+code).data("reagent",reagent);
-        
+
         //Add event listeners
         $("#r"+code).hover(hoverReagent);
         $("#r"+code).click(clickReagent);
-        
+
         //Register back references
         alchemyEffects[reagent.e1].reagents.push(code);
         alchemyEffects[reagent.e2].reagents.push(code);
         alchemyEffects[reagent.e3].reagents.push(code);
         alchemyEffects[reagent.e4].reagents.push(code);
     }
-    
+
     //Initially hide all effects spans in the reagents
     $("#reagents .effects span").hide();
 
@@ -161,11 +175,11 @@ var clearDetail = function() {
 
 var clickReagent = function(reagent) {
     var mode = $("#reagents").data("mode");
-    
+
     if(mode == "use") {
         useToggleReagent( this );
     }
-    
+
     if(mode == "activate") {
         activateToggleReagent( this );
     }
@@ -173,7 +187,7 @@ var clickReagent = function(reagent) {
 
 var toggleClickMode = function() {
     var mode = $("#reagents").data("mode");
-    
+
     if(mode == "use") {
         console.log("Going to disable mode");
         $("#reagents").data("mode", "activate");
@@ -184,11 +198,11 @@ var toggleClickMode = function() {
         $("#toggleMode").html("Potion Mode");
     }
 }
-    
+
 var useToggleReagent = function(clicked) {
     var nReagents = $("#selected").data("num");
     var code = $(clicked).data("code");
-    
+
     if( $(clicked).hasClass("in_potion") ) {
         $(clicked).removeClass("in_potion");
         $("#mixer ."+code).replaceWith("");
@@ -196,7 +210,7 @@ var useToggleReagent = function(clicked) {
         regeneratePotionEffects();
         return;
     }
-    
+
     if( $("#selected").data("num") < 3) {
         selectReagent(code, $(clicked).data("reagent"));
         $(clicked).addClass("in_potion");
@@ -215,7 +229,7 @@ var activateToggleReagent = function(clicked) {
         $(clicked).addClass("disabled");
         return;
     }
-    
+
     if( $(clicked).hasClass("disabled") ) {
         $(clicked).removeClass("disabled");
     } else {
@@ -248,7 +262,7 @@ var selectReagent = function(code, reagent) {
 
 var regeneratePotionEffects = function() {
     var nReagents = $("#selected").data("num");
-    
+
     //Reset
     $("#potion").empty();
     $("#reagents .positive").removeClass("positive");
@@ -256,13 +270,13 @@ var regeneratePotionEffects = function() {
     $("#reagents .effects span").hide();
     $("#effects .possible").removeClass("possible");
     $("#effects .in_potion").removeClass("in_potion");
-    
+
 	//Generate potion effects
     var potion = new Object();
 	for(code in alchemyEffects) {
 		potion[code] = 0;
 	}
-	
+
 	$("#mixer .reagent").each(function() {
 		var item = $(this).data("reagent");
 		potion[item.e1] = potion[item.e1] + 1;
@@ -270,7 +284,7 @@ var regeneratePotionEffects = function() {
 		potion[item.e3] = potion[item.e3] + 1;
 		potion[item.e4] = potion[item.e4] + 1;
 	});
-	
+
 	//Display results
 	for(code in alchemyEffects) {
 		if(potion[code] > 0) {
@@ -287,26 +301,20 @@ var regeneratePotionEffects = function() {
 	    	    $("#reagents .sp" + code).show();
 	    		$("#e" + code).addClass("possible");
 	    	}
-	    	
+
 	    	if(potion[code] > 1) {
 	    		$("#e" + code).addClass("in_potion");
 	    	}
 		}
 	}
-	
+
 	//Remove positive and negative from in_potion
 	$("#reagents .in_potion").removeClass("positive");
 	$("#reagents .in_potion").removeClass("negative");
-	
+
 	if(nReagents==3) {
 		$("#selected .inactive").hide();
 	} else {
 		$("#selected .inactive").show();
 	}
 }
-
-
-
-
-
-
